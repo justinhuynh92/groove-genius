be46d4736cdd1420d4bf6ed8a643dced70ac123e
Chong Vang <cvang530@gmail.com>
2023-08-21 12:47:28 -0700

    [Added] genre table into database

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -7,10 +7,16 @@ steps = [
             username VARCHAR(20) NOT NULL,
             password VARCHAR(20) NOT NULL
         );
+
+        CREATE TABLE genres (
+            name VARCHAR(30) NOT NULL,
+            id SERIAL PRIMARY KEY NOT NULL
+        );
         """,
         # "Down" SQL statement
         """
         DROP TABLE users;
-        """
+        DROP TABLE genres;
+        """,
     ],
 ]
