0599d923d52a33445f959ffc5de361337f9e10f5
Tyler Niemann <tntman98@gmail.com>
2023-08-21 14:16:35 -0700

    created tracks table/migration

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -12,11 +12,19 @@ steps = [
             name VARCHAR(30) NOT NULL,
             id SERIAL PRIMARY KEY NOT NULL
         );
+         CREATE TABLE tracks (
+            title VARCHAR(20) NOT NULL,
+            artist VARCHAR(20) NOT NULL,
+            album VARCHAR(30) NOT NULL,
+            genre_id INT  NOT NULL REFERENCES genres(id),
+            id SERIAL PRIMARY KEY NOT NULL
+        );
         """,
         # "Down" SQL statement
         """
         DROP TABLE users;
         DROP TABLE genres;
+        DROP TABLE tracks;
         """,
     ],
 ]
