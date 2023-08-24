288c54f08205e52ad6806af6d2d86b57fbe1004f
David Francisco <dfrancisco2677@gmail.com>
2023-08-24 19:09:53 +0000

    Get playlist endpoint

--- api/routers/playlists.py
+++ api/routers/playlists.py
@@ -15,4 +15,16 @@ async def create_playlist(
     playlist: Playlist, playlist_repo: PlaylistRepository = Depends()
 ):
     id = playlist_repo.create_playlist(playlist)
+    if id is None:
+        return {"message": "Could not create a playlist."}
     return {"id": id, **playlist.dict()}
+
+
+@router.get("/playlists/{user_id}", response_model=List[PlaylistOut])
+async def get_playlists(
+    user_id: int, playlist_repo: PlaylistRepository = Depends()
+):
+    playlists = playlist_repo.get_playlists(user_id)
+    if playlists is None:
+        return {"message": "Could not get playlists."}
+    return playlists
