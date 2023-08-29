bc6bb287a52901da621ea0b2145704c59f3007f1
TylerNiemann <tntman98@gmail.com>
2023-08-29 18:58:18 +0000

    update/delete user endpoints

--- api/routers/users.py
+++ api/routers/users.py
@@ -10,7 +10,7 @@ from jwtdown_fastapi.authentication import Token
 from authenticator import authenticator
 from pydantic import BaseModel
 from queries.users import UserRepository
-from models.users import UserIn, UserOut, UserOutWithPassword
+from models.users import UserIn, UserOut, UserOutWithPassword, User
 
 class AccountForm(BaseModel):
     username: str
@@ -37,3 +37,24 @@ async def create_account(
     form = AccountForm(username=info.username, password=info.password)
     token = await authenticator.login(response, request, form, repo)
     return AccountToken(account=account, **token.dict())
+
+@router.delete("/users/{id}", response_model=UserOut)
+async def delete_user(id: int, user_repo: UserRepository = Depends()):
+    deletion_successful = user_repo.delete_user(id)
+
+    if deletion_successful:
+        return {"message": "User deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+@router.put("/users/{id}", response_model=dict)
+async def update_user(
+    id: int,
+    user: User,
+    user_repo: UserRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    response = user_repo.update_user(id, user)
+    if response is None:
+        return {"message": "Could not change user"}
+    return response
