d2c3b0f304b68b41b923f9968332bcce89f1e2b8
Justin Huynh <justinhuynh639@gmail.com>
2023-09-07 16:37:50 -0700

    changes

--- ghi/src/UserProfile.js
+++ ghi/src/UserProfile.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import {Link} from 'react-router-dom'
 
 function ProfilePage() {
     const {token, logout, fetchWithToken} = useToken();
@@ -28,11 +29,17 @@ function ProfilePage() {
             <div className="bg-white shadow-md rounded-lg p-4">
                 <ul>
                     <li className="mb-2">
-                        <h2 className="text-lg font-semibold">Username</h2>
+                        <h2 className="text-lg font-semibold">My Library</h2>
                     </li>
-                    <li className="list-group-item border bg-light">Genre</li>
-                    <li className="list-group-item border bg-light">Tracks</li>
-                    <li className="list-group-item border bg-light">Playlists</li>
+                    <li className="list-group-item border bg-light">
+                        <Link to="/genres">Genre</Link>
+                    </li>
+                    <li className="list-group-item border bg-light">
+                        <Link to="/tracks">Tracks</Link>
+                    </li>
+                    <li className="list-group-item border bg-light">
+                        <Link to="/playlists">Playlists</Link>
+                    </li>
                 </ul>
             </div>
         </div>
