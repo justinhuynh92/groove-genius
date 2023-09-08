901ca6432e3759d45734114ca7da2f1ae210c24f
Tyler Niemann <tntman98@gmail.com>
2023-09-08 14:05:16 -0700

    change CORS

--- api/main.py
+++ api/main.py
@@ -11,9 +11,15 @@ app.include_router(genres.router)
 app.include_router(playlists.router)
 app.include_router(authenticator.router)
 
+origins = [
+    "http://localhost:3000",
+    os.environ.get("CORS_HOST", None),
+]
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
