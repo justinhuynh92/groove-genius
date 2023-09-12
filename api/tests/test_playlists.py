dc03933e34b2a998ead1266e09f840fc277f452f
David Francisco <dfrancisco2677@gmail.com>
2023-09-11 17:04:39 -0700

    URL and test changes.

--- api/tests/test_playlists.py
+++ api/tests/test_playlists.py
@@ -9,13 +9,54 @@ class EmptyPlaylistQueries:
     def get_playlists(self):
         return []
 
+    def get_playlist(self, id: int):
+        return {
+            "id": 1,
+            "name": "string",
+            "tracks": [
+                {
+                    "id": 1,
+                    "title": "string",
+                    "artist": "string",
+                    "album": "string",
+                    "genre": "ROCK",
+                },
+                {
+                    "id": 2,
+                    "title": "string",
+                    "artist": "string",
+                    "album": "string",
+                    "genre": "ROCK",
+                },
+            ],
+        }
+
 
 def test_get_all_playlists():
     app.dependency_overrides[PlaylistRepository] = EmptyPlaylistQueries
 
-    response = client.get("/playlists")
+    response = client.get("/playlists/1")
 
     app.dependency_overrides = {}
 
     assert response.status_code == 200
-    assert response.json() == {"playlists": []}
+    assert response.json() == {
+        "id": 1,
+        "name": "string",
+        "tracks": [
+            {
+                "id": 1,
+                "title": "string",
+                "artist": "string",
+                "album": "string",
+                "genre": "ROCK",
+            },
+            {
+                "id": 2,
+                "title": "string",
+                "artist": "string",
+                "album": "string",
+                "genre": "ROCK",
+            },
+        ],
+    }
